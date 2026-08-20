b929c8b7d4a7a2c41c14badebc5c277c0db20259
Duy <duylaptrinh03@gmail.com>
2026-08-20 11:39:35 +0700

    feat: wire OrdersController POST /orders route

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
+  imports: [MailModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
